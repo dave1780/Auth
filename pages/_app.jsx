b8822be3290d15c52f9b4b5b3c8a30b59162b2fd
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-15 15:52:00 +0545

    1 file modified, 1 added

---
+++ pages/_app.jsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return(
+    <>
+     <form className="form">
+       <li>Username: <input required/></li>
+       <li>Email: <input required /></li>
+       <li>Password: <input required /> </li>
+       <li>confirm_Password: <input required /></li>
+       <button>Sign up</button>
+     </form>
+     <Component {...pageProps} />
+     </>)
+}
+
+export default MyApp
